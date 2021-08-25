2c3e3a0a8e58fd70d5605471cc2e16b00d97a8da
ayafarid <73356697+ayafarid@users.noreply.github.com>
2021-08-25 23:46:45 +0200

    Add files via upload

---
+++ Assignments/ps0/ps0.py
@@ -0,0 +1,10 @@
+import math
+
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+
+
+
+print('X^Y = ', x**y)
+print('X/Y = ', x//y)
+print('logarithm of “{}” is = {}'.format(x, math.log(x,2)))
